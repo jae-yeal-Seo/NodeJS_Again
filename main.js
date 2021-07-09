c6cc7139836aa54a9eef9807b459d3864dbc0ca3
woduf9173@naver.com <woduf9173@naver.com>
2021-07-10 02:31:01 +0900

    id파라미터에서 value값 읽음을 이용해서 파일을 웹에 띄움

--- main.js
+++ main.js
@@ -5,39 +5,42 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
+    //url에서 query부분을 딴다.
+    var title = queryData.id;
+    //query부분에서 id의 파라미터를 title이라는 변수에 저장한다.
     if(_url == '/'){
-      _url = '/index.html';
+      _url = 'Welcome';
     }
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    var template = `
+    fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+      //★queryData에서 id라는 파라미터의 value값을 불러와 "data/해당value값"파일을 description이라는 변수를 통해 사용할 수 있게 된다. 
+      var template = `
     <!doctype html>
 <html>
 <head>
-  <title>WEB1 - ${queryData.id}</title>
+  <title>WEB1 - ${title}</title>
   <meta charset="utf-8">
 </head>
 <body>
   <h1><a href="index.html">WEB</a></h1>
   <ol>
-    <li><a href="1.html">HTML</a></li>
-    <li><a href="2.html">CSS</a></li>
-    <li><a href="3.html">JavaScript</a></li>
+    <li><a href="?id=HTML">HTML</a></li>
+    <li><a href="?id=CSS">CSS</a></li>
+    <li><a href="?id=JavaScript">JavaScript</a></li>
   </ol>
-  <h2>${queryData.id}</h2>
-  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-  <img src="coding.jpg" width="100%">
-  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-  </p>
+  <h2>${title}</h2>
+  <p>${description}</p>
 </body>
 </html>
     `
     response.end(template);
+    });
+    
     //해당 파일을 읽음
-    //response.end가 최종적으로 화면에 보여짐
+    //response.end가 최종적으로 화면에 보여짐<==이걸 까먹어서 한참 다른파일만 뒤졌다...
   
 });
 app.listen(3000);
